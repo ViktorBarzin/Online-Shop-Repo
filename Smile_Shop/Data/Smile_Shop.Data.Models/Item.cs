125170fad318272aaf36fd1fc4f6f260b2a9a9c9
ViktorBarzin <vbarzin@gmail.com>
2016-11-12 14:35:06 +0200

    added models

---
+++ Smile_Shop/Data/Smile_Shop.Data.Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Smile_Shop.Data.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+
+        public string PictureLocation { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        
+    }
+}
---
+++ Smile_Shop/Data/Smile_Shop.Data.Models/UserType.cs
@@ -0,0 +1,9 @@
+﻿namespace Smile_Shop.Data.Models
+{
+    public class UserType
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+    }
+}
