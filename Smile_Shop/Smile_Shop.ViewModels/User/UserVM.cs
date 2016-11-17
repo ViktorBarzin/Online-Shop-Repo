f0bac36cf44d9eed6d42a1b457dda891361ffa9f
ViktorBarzin <vbarzin@gmail.com>
2016-11-17 22:57:55 +0200

    more vms

--- Smile_Shop/Smile_Shop.ViewModels/User/UserVM.cs
+++ Smile_Shop/Smile_Shop.ViewModels/User/UserVM.cs
@@ -4,6 +4,7 @@
     using Smile_Shop.Data.Models;
     using Smile_Shop.Lozalization.Resources;
     using Smile_Shop.ViewModels.Infrastructure;
+    using System;
     using System.ComponentModel.DataAnnotations;
 
     public class UserVM : IMapFrom<ApplicationUser>
@@ -20,9 +21,15 @@
         [Display(Name = "Assignment", ResourceType = typeof(Resources))]
         public string Id { get; set; }
 
-        [Display(Name = "Name", ResourceType = typeof(Resources))]
+        [Display(Name = "FirstName", ResourceType = typeof(Resources))]
         [Required(ErrorMessageResourceName = "NameRequired", ErrorMessageResourceType = typeof(Resources))]
-        public string Name { get; set; }
+        [MinLength = 3]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName", ResourceType = typeof(Resources))]
+        [Required(ErrorMessageResourceName = "NameRequired", ErrorMessageResourceType = typeof(Resources))]
+        public string LastName { get; set; }
+
 
         [Display(Name = "Email", ResourceType = typeof(Resources))]
         [Required(ErrorMessageResourceName = "EmailRequired", ErrorMessageResourceType = typeof(Resources))]
@@ -30,5 +37,14 @@
         public string Email { get; set; }
 
         public string PasswordResetToken { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime LastLoginDate { get; set; }
+
+        public string City { get; set; }
+
     }
 }
